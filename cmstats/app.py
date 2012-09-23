b390b15fb6802cc6865aa52c69c3b5420a24014c
Chris Soyars <ctso@ctso.me>
2012-09-23 01:40:52 -0700

    Use tornado command line arguments instead of argparse

--- cmstats/app.py
+++ cmstats/app.py
@@ -1,4 +1,3 @@
-import argparse
 import logging
 import time
 import os
@@ -18,8 +17,6 @@ from threads import DatabaseThread
 
 define('port', 6543)
 define('debug', True)
-
-logging.basicConfig(level=logging.DEBUG)
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -54,27 +51,19 @@ class Application(tornado.web.Application):
             thread.start()
 
 def run_server():
-    parser = argparse.ArgumentParser(description="CMStats Server")
-    parser.add_argument('--port', dest='port', type=int, help="Port", default=6543)
-    parser.add_argument('--config', dest='config', type=unicode, help="Path to configuration file", default="/etc/cmstats.ini")
-    parser.add_argument('--logging', dest='logging', type=unicode, help="Logging Level", choices=['debug', 'info', 'warning', 'error', 'none'], default='debug')
-    args = parser.parse_args()
-
-    define('settings', '')
-    options.logging = args.logging
-    options.port = args.port
-    options.config = args.config
-
+    # Define command line options
+    define("config", default="/etc/cmstats.ini", type=unicode, help="Path to configuration file")
+    tornado.options.parse_command_line()
     app = Application()
+
     server = tornado.httpserver.HTTPServer(app, xheaders=True)
     server.listen(int(options.port))
     try:
         IOLoop.instance().start()
     except KeyboardInterrupt:
-        logging.debug("Shutting down all threads")
+        logging.info("Shutting down all threads")
         for thread in app.threads.itervalues():
             thread.running = False
-
 
 if __name__ == '__main__':
     run_server()

879eb959c8bd4781d30a012ab534ec18c3f4c0c6
Chris Soyars <ctso@ctso.me>
2012-09-22 19:44:38 -0700

    Re-enable database heavy queries

--- cmstats/views/index.py
+++ cmstats/views/index.py
@@ -7,14 +7,11 @@ from pyramid.view import view_config
 @view_config(context=Root, renderer='index.mako')
 def index(request):
     kwargs = {
-            #'device_count': Device.device_count(),
-            #'version_count': Device.version_count(),
+            'device_count': Device.device_count(),
+            'version_count': Device.version_count(),
             'total_nonkang': Device.count_nonkang(),
             'total_kang': Device.count_kang(),
-            #'total_last_day': Device.count_last_day(),
-            'device_count': [],
-            'version_count': [],
-            'total_last_day': -1,
+            'total_last_day': Device.count_last_day(),
     }
     
     return kwargs

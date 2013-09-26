4ec3989266256dd4a6e3ede0c4e5147a0941ca5d
Chris Soyars <ctso@ctso.me>
2013-09-25 17:36:34 -0700

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,23 +2,10 @@ from fabric.api import run, sudo, puts, abort, env, open_shell, local, put
 from fabric.colors import green, red, yellow
 import os
 
-
-if os.environ.get('TARDIS_HOME') is None:
-    puts(red("You must enter the Tardis first"))
-    puts("")
-    puts(red("pr0tip: "))
-    puts("")
-    puts(green("$ git clone git@github.com:CyanogenMod/tardis.git # Private Repository"))
-    puts(green("$ source tardis/bin/activate"))
-    abort("Unable to read TARDIS_HOME")
-
 env.use_ssh_config = True
-env.ssh_config_path = os.path.join(os.environ['TARDIS_HOME'], "config", "ssh_config")
-env.key_filename = os.path.join(os.environ['TARDIS_HOME'], "keys", "fab_rsa")
 
 def all():
-    env.user = "fabric"
-    env.hosts = ['8.29.134.126:22221']
+    env.hosts = ['stats01']
 
 def uptime():
     run('uptime')
